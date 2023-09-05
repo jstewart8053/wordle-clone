48e40aacc3d9c7d15d2e50d50c9eab9e9a51c58c
Jennifer Stewart <jstewart8053@gmail.com>
2023-09-04 20:27:23 -0400

    add coming soon!

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -4,7 +4,7 @@ import {useState} from 'react';
 function Keyboard() {
     return (
 <div>
-
+Keyboard
 </div>
     )
 }
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -5,11 +5,10 @@ import { AppContext } from "../App";
 function Letter({letterPos, attemptVal}) {
   const {board} = useContext(AppContext) 
     const letter = board[attemptVal][letterPos] ;
-    console.log(letter)
 
     return (
         <div>
-<div className= "letter"></div>
+<div className= "letter">{letter}</div>
 
         </div>
     )
