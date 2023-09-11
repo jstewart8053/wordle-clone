13200aae6a8de2d118b630c6d945b12e9d9c2970
Jennifer Stewart <jstewart8053@gmail.com>
2023-09-11 17:04:56 -0400

    Created Keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,8 +2,16 @@ import {useState} from 'react';
 
 
 function Keyboard() {
-    return (
-<div>
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+ 
+ return (
+<div className='keyboard'>
+    <div className='line1'></div>
+    <div className='line2'></div>
+    <div className='line3'></div>
+
 Keyboard
 </div>
     )
